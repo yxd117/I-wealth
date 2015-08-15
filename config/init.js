8075a3481cc9e3232e692a1c40c12282b8f670ce
Izzuddin <izzuddin.y@gmail.com>
2015-08-15 23:55:25 +0800

    update

--- config/init.js
+++ config/init.js
@@ -25,13 +25,13 @@ module.exports = function() {
 			}
 
 			process.env.NODE_ENV = 'development';
-			// process.env.AWS_ACCESS_KEY = 'AKIAJHM7JD2AHUEJYAVQ';
-			// process.env.AWS_SECRET_KEY = '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';
-			// process.env.S3_BUCKET = 'nodeup';
+			process.env.AWS_ACCESS_KEY = 'AKIAJHM7JD2AHUEJYAVQ';
+			process.env.AWS_SECRET_KEY = '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';
+			process.env.S3_BUCKET = 'nodeup';
 			//shiqi
-			process.env.AWS_ACCESS_KEY = 'AKIAJ3CE2YJZPPQSHZKQ';
-			process.env.AWS_SECRET_KEY = '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';
-			process.env.S3_BUCKET = 'hexapic';
+			// process.env.AWS_ACCESS_KEY = 'AKIAJ3CE2YJZPPQSHZKQ';
+			// process.env.AWS_SECRET_KEY = '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';
+			// process.env.S3_BUCKET = 'hexapic';
 		}
 	});
 
