41ad63a5453b9865e41f2c853286255fdfaf01b9
Izzuddin <izzuddin.y@gmail.com>
2015-10-05 18:37:47 +0800

    switch db

--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,8 @@
 module.exports = {
 	db: {
 		//uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
-		uri: 'mongodb://admin:poppop123@ds031893.mongolab.com:31893/hexastg',
+		//uri: 'mongodb://admin:poppop123@ds031893.mongolab.com:31893/hexastg',
+		uri: ' mongodb://admin:poppop123@ds029804.mongolab.com:29804/fyphexa',
 		options: {
 			user: '',
 			pass: ''
